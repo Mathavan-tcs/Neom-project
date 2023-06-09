8dc288bf2dbeb73950e30f4d9ede514a578afef5
1936119 <1936119@tcs.com>
2023-06-09 11:57:05 +0530

    adding build.gradle files.

--- app/src/main/java/com/tcs/coffeeapp/CoffeeListFragment.kt
+++ app/src/main/java/com/tcs/coffeeapp/CoffeeListFragment.kt
@@ -8,30 +8,21 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.tcs.coffeeapp.databinding.FragmentCoffeelistBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class CoffeeListFragment : Fragment() {
 
     private var _binding: FragmentCoffeelistBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentCoffeelistBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_CoffeeListFragment_to_CoffeeDetailsFragment)
         }
--- app/src/main/java/com/tcs/coffeeapp/CoffeeMainActivity.kt
+++ app/src/main/java/com/tcs/coffeeapp/CoffeeMainActivity.kt
@@ -2,10 +2,13 @@ package com.tcs.coffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tcs.coffeeapp.databinding.ActivityCoffeemainBinding
 
 class CoffeeMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCoffeemainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coffeemain)
+        binding = ActivityCoffeemainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
