620dc254b06230a398471fe906343e338015d6de
1936119 <1936119@tcs.com>
2023-06-14 19:47:23 +0530

    Removed unused constant value

--- app/src/main/java/com/tcs/coffeeapp/data/api/CoffeeService.kt
+++ app/src/main/java/com/tcs/coffeeapp/data/api/CoffeeService.kt
@@ -2,7 +2,6 @@ package com.tcs.coffeeapp.data.api
 
 import com.tcs.coffeeapp.data.model.Coffee
 import com.tcs.coffeeapp.utils.Constants.HOT_COFFEES_ENDPOINT
-import com.tcs.coffeeapp.utils.Constants.ICED_COFFEES_ENDPOINT
 import retrofit2.http.GET
 
 interface CoffeeService {
