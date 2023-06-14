d8d412a54815a33df522fe187caa5cc082193c27
1936119 <1936119@tcs.com>
2023-06-14 19:42:30 +0530

    Removed unused methods.

--- app/src/main/java/com/tcs/coffeeapp/utils/Constants.kt
+++ app/src/main/java/com/tcs/coffeeapp/utils/Constants.kt
@@ -3,5 +3,4 @@ package com.tcs.coffeeapp.utils
 object Constants {
     const val COFFEE_BASE_API_URL = "https://api.sampleapis.com/"
     const val HOT_COFFEES_ENDPOINT = "coffee/hot"
-    const val ICED_COFFEES_ENDPOINT = "coffee/iced"
 }
