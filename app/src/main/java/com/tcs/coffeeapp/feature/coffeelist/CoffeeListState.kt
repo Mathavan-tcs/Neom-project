ae16decda64dbffcb3175372df6e9e149f713d2a
1936119 <1936119@tcs.com>
2023-07-11 10:46:09 +0530

    coffee_list: adding documentation part.

--- app/src/main/java/com/tcs/coffeeapp/feature/coffeelist/CoffeeListState.kt
+++ app/src/main/java/com/tcs/coffeeapp/feature/coffeelist/CoffeeListState.kt
@@ -3,6 +3,11 @@ package com.tcs.coffeeapp.feature.coffeelist
 import com.tcs.coffeeapp.data.model.Coffee
 import com.tcs.coffeeapp.feature.base.IViewState
 
+/**
+ *  @param isLoading - showing progress bar and making it as default status as false.
+ *  @param coffees - populating list of coffees.
+ *  @param error - maintaining error message.
+ */
 data class CoffeeListState(
     val isLoading: Boolean = false,
     val coffees: List<Coffee>? = emptyList(),
